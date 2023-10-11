f6b06637d7dc0aae7417bbd067275ad6fa048098
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2023-10-11 17:19:43 -0400

    fix: added install from template
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

---
+++ install-from-template.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+read -rp "GitHub Username: " user
+read -rp "Projectname: " projectname
+
+git clone git@github.com:AndriyKalashnykov/go-kafka-confluent-examples.git "$projectname"
+cd "$projectname"
+rm -rf .git
+find . -type f -exec sed -i "s/go-kafka-confluent-examples/$projectname/g" {} +
+find . -type f -exec sed -i "s/[Aa]ndriy[Kk]alashnykov/$user/g" {} +
+git init
+git add .
+git commit -m "initial commit"
+git remote add origin "git@github.com:$user/$projectname.git"
+
+echo "Template successfully installed."
+
+exit 0
