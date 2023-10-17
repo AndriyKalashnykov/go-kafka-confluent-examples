025efaf3ecf1fb1b6da53f7c89d1b717dd55de28
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2023-10-16 22:04:53 -0400

    fix: updates
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- scripts/install-osxcross-ubuntu.sh
+++ scripts/install-osxcross-ubuntu.sh
@@ -33,6 +33,10 @@ UNATTENDED=1 PORTABLE=true ./build.sh
 UNATTENDED=1 PORTABLE=true ./build_gcc.sh
 UNATTENDED=1 PORTABLE=true ./build_apple_clang.sh*
 
+# repo info
+# https://api.github.com/repos/AndriyKalashnykov/osxcross-target
+
+#git lfs install
 #git lfs migrate import --include=bin/x86_64-apple-darwin23-lto-dump
 #git lfs migrate import --include=libexec/gcc/x86_64-apple-darwin23/13.2.0/cc1
 #git lfs migrate import --include=libexec/gcc/x86_64-apple-darwin23/13.2.0/cc1obj
@@ -40,6 +44,24 @@ UNATTENDED=1 PORTABLE=true ./build_apple_clang.sh*
 #git lfs migrate import --include=libexec/gcc/x86_64-apple-darwin23/13.2.0/cc1objplus
 #git lfs migrate import --include=libexec/gcc/x86_64-apple-darwin23/13.2.0/cc1plus
 #git lfs migrate import --include=libexec/gcc/x86_64-apple-darwin23/13.2.0/lto1
+
+#git lfs uninstall
+#git lfs untrack bin/x86_64-apple-darwin23-lto-dump
+#git lfs untrack libexec/gcc/x86_64-apple-darwin23/13.2.0/cc1
+#git lfs untrack libexec/gcc/x86_64-apple-darwin23/13.2.0/cc1obj
+#git lfs untrack libexeclibexec/gcc/x86_64-apple-darwin23/13.2.0/cc1objplus
+#git lfs untrack libexec/gcc/x86_64-apple-darwin23/13.2.0/cc1objplus
+#git lfs untrack libexec/gcc/x86_64-apple-darwin23/13.2.0/cc1plus
+#git lfs untrack libexec/gcc/x86_64-apple-darwin23/13.2.0/lto1
+#git lfs status
+#git lfs prune
+#git lfs push origin main
+#git filter-branch --force --index-filter \
+#  "git rm --cached --ignore-unmatch x86_64-apple-darwin23-lto-dump cc1 cc1obj cc1objplus cc1objplus cc1plus lto1" \
+#  --prune-empty --tag-name-filter cat -- --all
+#git reflog expire --expire=now --all && git gc --prune=now --aggressive
+#git push origin main --force
+#git lfs ls-files
 
 echo "Don't forget to add to the PATH"
 echo 'export PATH=$PATH:$ROOTDIR/$OSXCROSSDIR/target/bin'
